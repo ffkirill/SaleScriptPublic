341d2f8cde86fbd8d4aa9232e696bbe1a5b3605d
ffkirill <ff.kirill@gmail.com>
2018-02-28 01:55:09 +0300

    Updated docker configs

--- server/update.sh
+++ server/update.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 docker-compose build
 docker-compose up -d
-docker-compose run --rm web /usr/local/bin/python manage.py migrate
-docker-compose run --rm web /usr/local/bin/python manage.py collectstatic --noinput
-docker-compose run --rm web /usr/local/bin/python manage.py mtime_cache --clean; docker-compose run --rm web /usr/local/bin/python manage.py compress
-docker-compose restart web
 
